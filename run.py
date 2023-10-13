2cde832306fa031602fba336d743469748ba21f8
alanpaterson82 <>
2023-10-13 14:16:26 +0000

    Add commas to word list

--- run.py
+++ run.py
@@ -2,16 +2,39 @@ import random
 name = input("Type your name: ")
 print("Good luck!", name)
 words = [
-    "Tiger"
-    "Lion"
-    "Elephant"
-    "Giraffe"
-    "Hippopotamus"
-    "Antelope"
-    "Zebra"
-    "Hyena"
-    "Rhinoceros"
-    "Bear"
-    "Leopard"
+    "Tiger",
+    "Lion",
+    "Elephant",
+    "Giraffe",
+    "Hippopotamus",
+    "Antelope",
+    "Zebra",
+    "Hyena",
+    "Rhinoceros",
+    "Bear",
+    "Leopard",
 ]
-
+word = random.choice(words)
+print("\nChoose a letter")
+guesses = ""
+tries = 10
+while tries > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end = "")
+        else:
+            print("_", end = "")
+            failed += 1
+    if failed == 0:
+        print("\n\nCongratulations!")
+        print("\nThe word was:", word)
+        break
+    guess = input("\n\nChoose a letter: ")
+    guesses += guess
+    if guess not in word:
+        tries -= 1
+        print("\nThat's Not Right")
+        print("\nYou have", +tries, "remaining")
+        if tries == 0:
+            print("\n\nBetter Luck Next Time!")
