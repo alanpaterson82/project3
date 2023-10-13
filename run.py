4bede54d1edfd32cbcf70f6494403500bd41b718
alanpaterson82 <>
2023-10-13 14:03:12 +0000

    Add list of random words

--- run.py
+++ run.py
@@ -1,2 +1,17 @@
 import random
 name = input("Type your name: ")
+print("Good luck!", name)
+words = [
+    "Tiger"
+    "Lion"
+    "Elephant"
+    "Giraffe"
+    "Hippopotamus"
+    "Antelope"
+    "Zebra"
+    "Hyena"
+    "Rhinoceros"
+    "Bear"
+    "Leopard"
+]
+
